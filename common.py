bd198eff55e9867758cd9ff467d361a22821f067
matthaeusheer <matthaeusheer@gmail.com>
2023-05-02 23:08:45 +0200

    added data loading capabilities

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
-PROJECT_ROOT_PATH = os.path.dirname((__file__))
-DATA_DIR_PATH = os.path.join(PROJECT_ROOT_PATH, 'data')
+PROJECT_ROOT_PATH = Path(os.path.dirname((__file__)))
+DATA_DIR_PATH = Path(os.path.join(PROJECT_ROOT_PATH, 'data'))
--- kolihack/io.py
+++ kolihack/io.py
@@ -1,4 +1,12 @@
+import pandas as pd
+
+from common import DATA_DIR_PATH
 
 
-def hi():
-    print('hi')
+def load_content_file() -> pd.DataFrame:
+    try:
+        df = pd.read_csv(DATA_DIR_PATH / 'content.csv')
+        print(f'Successfully loaded pandas data frame from {DATA_DIR_PATH}/content.csv')
+        return df
+    except FileNotFoundError as error:
+        print(f'Could not load content file. Did you download it to "data/content.csv"? {error}')
