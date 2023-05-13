b18d7104b7d3564ec2a314c88f60bf5c809a5dfe
Yumin Sun <yuminsun@outlook.com>
2023-05-13 18:11:54 +0200

    update my query test

--- kolihack/query.py
+++ kolihack/query.py
@@ -1,6 +1,6 @@
 import argparse
 from bert_search import BertSearch
-from io import load_pkl_from_file
+from kolihack.io import load_pkl_from_file
 
 def main():
     parser = argparse.ArgumentParser(description="Query a search term, return the id of the content.")
@@ -10,7 +10,6 @@ def main():
 
     assert(len(args.query) > 0)
 
-if __name__ == "__main__":
     input_embeddings = load_pkl_from_file("../data/embeddings_by_id.pkl")
     input_id_and_text = load_pkl_from_file("../data/input_id_and_text.pkl")
     print("Dear Kolibri User  - what are you looking for?")
